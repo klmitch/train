11878c774545e76fc119ae8525fb4520ce070afb
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2013-05-17 16:23:21 -0500

    Add a process launcher to utilities.

--- train/util.py
+++ train/util.py
@@ -14,6 +14,12 @@
 #    under the License.
 
 import collections
+import logging
+import os
+import signal
+
+
+LOG = logging.getLogger(__name__)
 
 
 # A sentinel used to signal that an argument was not passed in
@@ -167,3 +173,107 @@ class StackedDict(collections.MutableMapping):
         """
 
         return dict(self.iteritems())
+
+
+class SignalExit(SystemExit):
+    """
+    A subclass of ``SystemExit`` which will include the signal number
+    that was raised to cause the server to exit.
+    """
+
+    def __init__(self, signo, exccode=1):
+        """
+        Initialize a ``SignalExit`` object.
+
+        :param signo: The signal that was received.
+        :param exccode: The status code to exit with.
+        """
+
+        super(SignalExit, self).__init__(exccode)
+        self.signo = signo
+
+
+class Launcher(object):
+    """
+    A class that assists in launching a server.
+    """
+
+    # Map signal codes to signal names...
+    signames = dict((v, k) for k, v in signal.__dict__.items()
+                    if k.startswith('SIG') and not k.startswith('SIG_'))
+
+    # Signals that should kill the child
+    death_sigs = set([signal.SIGTERM, signal.SIGINT])
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize a ``Launcher`` object.  The first positional
+        argument must be a callable that starts the server.  Remaining
+        positional and keyword arguments will be passed to the start
+        function after creating the child.
+        """
+
+        # Make sure we have at least one positional argument
+        if not args:
+            raise TypeError("Launcher requires at least one argument")
+
+        self.start_func = args[0]
+        self.start_args = args[1:]
+        self.start_kwargs = kwargs
+
+    def _install_handler(self, handler):
+        """
+        Install the signal handler for the death signals.
+
+        :param handler: The signal handler to install.
+        """
+
+        for sig in self.death_sigs:
+            signal.signal(sig, handler)
+
+    def _handle_signal(self, signo, frame):
+        """
+        The signal handler for launched children.  Raises a
+        ``SignalExit`` to exit the child.
+
+        :param signo: The signal that was caught.
+        :param frame: The frame during which the signal was caught.
+        """
+
+        # Restore the default behavior on the death signals
+        self._install_handler(signal.SIG_DFL)
+
+        raise SignalExit(signo)
+
+    def start(self):
+        """
+        Launch the server.  This forks a new process; in the parent
+        process, the process ID of the child is returned.  This method
+        does not return in the child process.
+        """
+
+        pid = os.fork()
+        if pid != 0:
+            # Return the child's PID
+            return pid
+
+        # OK, this code executes in the child; let's start by
+        # installing the signal handlers
+        self._install_handler(self._handle_signal)
+
+        # Now call the starter
+        status = 0
+        try:
+            self.start_func(*self.start_args, **self.start_kwargs)
+        except SignalExit as exc:
+            LOG.info("%s: Caught %s, exiting" %
+                     (pid, self.signames[exc.signo]))
+            status = exc.code
+        except SystemExit as exc:
+            status = exc.code
+        except BaseException:
+            LOG.exception("%s: Unhandled exception" % pid)
+            status = 2
+
+        # Let's exit safely
+        os._exit(status)
